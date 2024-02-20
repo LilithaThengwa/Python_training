2013d1b3f097a1a1fc55330d9d90b1905ebe56e3
lilithathengwa <32202291-lilithathengwa@users.noreply.replit.com>
2024-02-20 06:46:59 +0000

    Added 2

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ c = int(completed)
 d = "=" * int(c/10)
 rem = 10 - c
 print(f"[{d}]")
+
+per = int(input("Please enter the percentage: "))
+
+eq = per // 10
+
+print(f"Output: [{('=' * eq) + ' ' * (10 - eq)}] {per}%")
+
+radius = float(input("Enter radius of circle: "))
+
+area = 3.14 * radius ** 2
+
+print(area)
