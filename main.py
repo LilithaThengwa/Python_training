2bcb3784035da7d464a72b9e8a1d7c08d2ee916c
LilithaThengwa <130540644+LilithaThengwa@users.noreply.github.com>
2024-02-20 06:57:03 +0000

    Fixed?

--- main.py
+++ main.py
@@ -4,17 +4,11 @@ d = "=" * int(c/10)
 rem = 10 - c
 print(f"[{d}]")
 
-per = int(input("Please enter the percentage: "))
+perc = int(input("enter the percentage: "))
 
-eq = per // 10
+x = perc // 10
 
-print(f"Output: [{('=' * eq) + ' ' * (10 - eq)}] {per}%")
-
-# radius = float(input("Enter radius of circle: "))
-
-# area = 3.14 * radius ** 2
-
-# print(area)
+print(f"Output: [{('=' * x) + ' ' * (10 - x)}] {perc}%")
 
 r = input("Enter: ")
 area = 3.14 * float(r) ** 2
