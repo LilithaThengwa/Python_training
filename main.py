9b4e5dc6b36a7bbdb5a26e963034054c8aea27ee
LilithaThengwa <130540644+LilithaThengwa@users.noreply.github.com>
2024-02-20 06:53:00 +0000

    All added for now

--- main.py
+++ main.py
@@ -10,8 +10,22 @@ eq = per // 10
 
 print(f"Output: [{('=' * eq) + ' ' * (10 - eq)}] {per}%")
 
-radius = float(input("Enter radius of circle: "))
+# radius = float(input("Enter radius of circle: "))
 
-area = 3.14 * radius ** 2
+# area = 3.14 * radius ** 2
 
+# print(area)
+
+r = input("Enter: ")
+area = 3.14 * float(r) ** 2
 print(area)
+
+year = input("year of birth? ")
+
+age = 2024 - int(year)
+
+print(f"Your age is {age}")
+
+temp= input("Enter temp: ")
+temp = float(temp)
+print((temp-32)*( 5/9))
