d09c0dee0dbbe2d222c11d0df54b9e897acdf434
LilithaThengwa <lilithathengwa@gmail.com>
2024-02-29 17:14:00 +0200

    h

--- Day8_assignment.py
+++ Day8_assignment.py
@@ -1,16 +1,4 @@
-# class Bank:
-
-#     def __init__(self, acc):
-#         self.acc = acc
-
-#     @property
-#     def name(self): # getter method
-#         return self._acc
-
-#     @name.setter
-#     def name(self, acc): # setter method
-#         self._acc = acc
-
+#A decorator for turning strings to uppercase when printing.(own decorator)
 from functools import wraps
 
 def Adecorator(func):
@@ -26,4 +14,31 @@ def What(name):
 
 print(What("joe"))
 
-#A decorator for turning strings to uppercase when printing.
+#==============================================================================================================================
+
+# # @property
+# - A built-in decorator in Python. 
+# - Helpful when defining properties with minimal effortwithout manually calling the property() function.
+# - Simplifies the usage of getters and setters.
+
+# ## example
+
+# class Bank:
+
+#     def __init__(self, acc):
+#         self.acc = acc
+
+#     @property
+#     def name(self): # getter method
+#         return self._acc
+
+#     @name.setter
+#     def name(self, acc): # setter method
+#         self._acc = acc
+
+# # Setter
+# - A method that makes it possbible to define a setter method for an attribute in a class.
+# - Modifies the value in a controlled way.
+
+# ## example
+# - Included above.
